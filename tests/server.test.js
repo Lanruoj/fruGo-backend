d64e062d471da5071fa5555e90b66a991150482f
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-06 11:54:38 +1100

    Create server test on [GET] /

---
+++ tests/server.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const { app } = require("../src/server");
+
+// Test that [GET] / index page works
+describe("Server request", () => {
+  describe("GET / index route", () => {
+    it("Returns successful status code", async () => {
+      const response = await request(app).get("/");
+      expect(response.statusCode).toEqual(200);
+    });
+  });
+});
