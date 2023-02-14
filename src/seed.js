745476fd4ced2689cedad185eeb0bba84c8af738
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-14 16:30:05 +1100

    Create cities & customers seed objects

---
+++ src/seed.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const { connectDatabase, disconnectDatabase } = require("./database");
+const { hashString } = require("./controllers/UserFunctions");
+const { City } = require("./models/City");
+
+const cities = [
+  { name: "Melbourne", state: "Victoria" },
+  { name: "Sydney", state: "New South Wales" },
+  { name: "Canberra", state: "Australian Capital Territory" },
+  { name: "Darwin", state: "Northern Territory" },
+  { name: "Perth", state: "Western Australia" },
+  { name: "Adelaide", state: "South Australia" },
+  { name: "Hobart", state: "Tasmania" },
+];
+
+const customers = [
+  {
+    email: "john_smith@email.com",
+    password: null,
+    username: "john_smith",
+    firstName: "John",
+    lastName: "Smith",
+    city: null,
+    streetAddress: "1234 John street",
+  },
+  {
+    email: "sally_smith@email.com",
+    password: null,
+    username: "sally_smith",
+    firstName: "Sally",
+    lastName: "Smith",
+    city: null,
+    streetAddress: "1234 Sally street",
+  },
+];
