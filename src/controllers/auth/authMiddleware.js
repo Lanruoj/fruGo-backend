0b78ebe5fc44ebb548406d1a7f265c135ed78eec
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-14 15:17:37 +1100

    Create authenticateUser() middleware

--- src/controllers/auth/authMiddleware.js
+++ src/controllers/auth/authMiddleware.js
@@ -1,4 +1,5 @@
 const { isEmail } = require("validator");
+const { verifyJWT } = require("./authHelpers");
 
 function validateEmail(request, response, next) {
   if (!isEmail(request.body.email)) {
@@ -8,4 +9,14 @@ function validateEmail(request, response, next) {
   next();
 }
 
-module.exports = { validateEmail };
+async function authenticateUser(request, response, next) {
+  let verifiedToken;
+  try {
+    verifiedToken = verifyJWT(request.body["authorization"]);
+    next();
+  } catch (error) {
+    return next(new Error("Invalid access token"));
+  }
+}
+
+module.exports = { validateEmail, authenticateUser };
