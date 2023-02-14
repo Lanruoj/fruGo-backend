855d56af70b411c62d2afb2bf10d6f8268ff26a3
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-14 12:19:18 +1100

    Configure crypto & create encryptString()

--- src/controllers/middleware/auth.js
+++ src/controllers/middleware/auth.js
@@ -1,5 +1,19 @@
 const { isEmail } = require("validator");
 
+// Encryption/decryption configuration
+const crypto = require("crypto");
+const encAlgorithm = "aes-256-cbc";
+const encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, "SpecialSalt", 32);
+const encIV = crypto.scryptSync(process.env.ENC_IV, "SpecialSalt", 16);
+let cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+let decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
+
+function encryptString(data) {
+  cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+
+  return cipher.update(data, "utf8", "hex") + cipher.final("hex");
+}
+
 function validateEmail(request, response, next) {
   if (!isEmail(request.body.email)) {
     return next(new Error("Invalid email address"));
@@ -7,5 +21,7 @@ function validateEmail(request, response, next) {
 
   next();
 }
+
+function generateJWT(request, response, next) {}
 
 module.exports = { validateEmail };
