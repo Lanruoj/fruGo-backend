33fd7b723f7f8f9752be4a2d25b5f53a70398752
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-15 15:41:59 +1100

    Use try/catch in customerRoutes

--- src/controllers/customers/CustomerRoutes.js
+++ src/controllers/customers/CustomerRoutes.js
@@ -5,16 +5,20 @@ const { validateEmail } = require("../auth/authMiddleware");
 const { generateAccessToken } = require("../auth/authHelpers");
 
 router.post("/register", validateEmail, async (request, response) => {
-  const newCustomer = await createCustomer({
-    email: request.body.email,
-    password: request.body.password,
-    username: request.body.password,
-    firstName: request.body.firstName,
-    lastName: request.body.lastName,
-    streetAddress: request.body.streetAddress,
-    city: request.body.city,
-  });
-
+  let newCustomer;
+  try {
+    newCustomer = await createCustomer({
+      email: request.body.email,
+      password: request.body.password,
+      username: request.body.password,
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      streetAddress: request.body.streetAddress,
+      city: request.body.city,
+    });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
   const accessToken = await generateAccessToken({ customer: newCustomer._id });
 
   response
