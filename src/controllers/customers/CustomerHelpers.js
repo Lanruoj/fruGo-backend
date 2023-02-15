05c5500809450aae0679fb0c2fd108f510576a59
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-15 15:41:25 +1100

    Create customerEmailExists() helper & use in createCustomer()

--- src/controllers/customers/CustomerHelpers.js
+++ src/controllers/customers/CustomerHelpers.js
@@ -1,7 +1,15 @@
 const { Customer } = require("../../models/Customer");
-const { hashString } = require("../auth/authHelpers");
+const { hashString, emailExists } = require("../auth/authHelpers");
+
+async function customerEmailExists(email) {
+  const foundEmail = await Customer.findOne({ email: email }).exec();
+  return foundEmail;
+}
 
 async function createCustomer(data) {
+  if (await customerEmailExists(data.email))
+    throw new Error("Email is already in use");
+
   const customerData = {
     email: data.email,
     password: await hashString(data.password),
