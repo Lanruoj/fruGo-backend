4555873fe9b8e0e1f7de56d59e65623e8caf8923
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-15 13:13:37 +1100

    [FIX] move timestamps option as options object

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-  cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart", required: true },
-  timestamps: true,
-  status: {
-    type: String,
-    default: "pending",
-    enum: ["pending", "complete", "cancelled"],
+const OrderSchema = new mongoose.Schema(
+  {
+    cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart", required: true },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "complete", "cancelled"],
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", OrderSchema);
 
