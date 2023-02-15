00d7d54fd216e476788808e1bc2847ce55193f0c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-15 13:09:06 +1100

    Add enum values for status

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema({
   cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart", required: true },
   timestamps: true,
-  status: { type: String, default: "pending" },
+  status: {
+    type: String,
+    default: "pending",
+    enum: ["pending", "complete", "cancelled"],
+  },
 });
 
 const Order = mongoose.model("Order", OrderSchema);
