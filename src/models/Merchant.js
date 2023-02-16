4dc6226e38ef02036c3ff7f173b20b160ee5f7bc
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-17 09:55:01 +1100

    Add username validation

--- src/models/Merchant.js
+++ src/models/Merchant.js
@@ -10,7 +10,16 @@ const MerchantSchema = new mongoose.Schema({
     required: true,
     minLength: [6, "Password must be greater than 6 characters long"],
   },
-  username: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (username) => {
+        return validator.isAlphanumeric(username, ["en-US"], { ignore: "_-" });
+      },
+      message: "Username may only contain alphanumeric characters",
+    },
+  },
   name: { type: String, required: true },
   description: { type: String, required: true },
   streetAddress: { type: String, required: true },
