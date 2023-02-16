6996b4bda465bfdcbb9cada0beb2068781e60175
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-16 15:12:28 +1100

    Create pre-save hook that hashes password

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
+const { hashString } = require("../controllers/auth/authHelpers");
 
 const CustomerSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+  },
   username: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   city: { type: mongoose.Schema.Types.ObjectId, ref: "City", required: true },
   streetAddress: { type: String, required: true },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+});
+
+CustomerSchema.pre("save", async function () {
+  // Hash password prior to saving
+  this.password = await hashString(this.password);
 });
 
 const Customer = mongoose.model("Customer", CustomerSchema);
