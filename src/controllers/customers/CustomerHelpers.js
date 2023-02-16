8d37820bddb2dbea172fb0d9d6725958d092da50
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-16 16:29:55 +1100

    Remove emailExists validation for createCustomer()

--- src/controllers/customers/CustomerHelpers.js
+++ src/controllers/customers/CustomerHelpers.js
@@ -1,18 +1,9 @@
 const { Customer } = require("../../models/Customer");
-const { hashString, emailExists } = require("../auth/authHelpers");
-
-async function customerEmailExists(email) {
-  const foundEmail = await Customer.findOne({ email: email }).exec();
-  return foundEmail;
-}
 
 async function createCustomer(data) {
-  if (await customerEmailExists(data.email))
-    throw new Error("Email is already in use");
-
   const customerData = {
     email: data.email,
-    password: await hashString(data.password),
+    password: data.password,
     username: data.username,
     firstName: data.firstName,
     lastName: data.lastName,
