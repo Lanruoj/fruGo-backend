356ee7519ae8b29e7b78e9d6baede8314d5e8597
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-16 17:10:43 +1100

    Remove /customers tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -22,26 +22,6 @@ describe("Test routes", () => {
   });
 });
 
-describe("Customer routes", () => {
-  describe("[POST] /register - register a new customer", () => {
-    it("Registers a new customer", async () => {
-      const city = await City.findOne({}).exec();
-      const response = await request(app).post("/customers/register").send({
-        email: "test123@email.com",
-        password: "testpassword123",
-        username: "test_username",
-        firstName: "Test",
-        lastName: "Lastname",
-        city: city._id,
-        streetAddress: "123 Test street",
-      });
-      expect(response.statusCode).toEqual(201);
-      expect(response.body.customer._id).toBeDefined();
-      expect(response.body.accessToken).toBeTruthy();
-    });
-  });
-});
-
 afterAll((done) => {
   done();
 });
