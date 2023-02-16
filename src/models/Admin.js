33d4117e475869d11f303a41b26c7925b9fdfc1e
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-17 10:02:30 +1100

    Add password validation

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const uniqueValidator = require("mongoose-unique-validator");
+const { hashString } = require("../controllers/auth/authHelpers");
 
 const AdminSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+    minLength: [6, "Password must be greater than 6 characters long"],
+  },
   username: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
