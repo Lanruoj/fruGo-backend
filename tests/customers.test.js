6647115bafa0345fdd364f8f3165c4140f68b818
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-17 16:24:38 +1100

    Setup mock auth for [GET] / test

--- tests/customers.test.js
+++ tests/customers.test.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const request = require("supertest");
 const mongoose = require("mongoose");
 const { app } = require("../src/server");
 const { City } = require("../src/models/City");
+const { Admin } = require("../src/models/Admin");
 const { seedDatabase } = require("../src/seed");
+const { generateAccessToken } = require("../src/controllers/auth/authHelpers");
 
 describe("Customer routes", () => {
   beforeAll(async () => {
@@ -27,6 +30,21 @@ describe("Customer routes", () => {
       expect(response.statusCode).toEqual(201);
       expect(response.body.customer._id).toBeDefined();
       expect(response.body.accessToken).toBeTruthy();
+    });
+  });
+
+  describe("[GET] / - get all customers", () => {
+    let accessToken = "";
+    let response;
+    beforeAll(async () => {
+      const admin = await Admin.findOne({ username: "test_admin" }).exec();
+      accessToken = await generateAccessToken(admin._id);
+      response = await request(app)
+        .get("/customers/")
+        .set("Authorization", `Bearer ${accessToken}`);
+    });
+    it("Gets all customers", async () => {
+      expect(response.statusCode).toEqual(200);
     });
   });
   afterAll(async () => {
