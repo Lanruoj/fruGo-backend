d76b57659e0d0b5c118b41da6a8597d6c920fc02
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-17 11:51:20 +1100

    Create [GET] / (no auth)

--- src/controllers/customers/CustomerRoutes.js
+++ src/controllers/customers/CustomerRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createCustomer } = require("./CustomerHelpers");
-// const { validateEmail } = require("../auth/authMiddleware");
+const { createCustomer, getAllCustomers } = require("./CustomerHelpers");
 const { generateAccessToken } = require("../auth/authHelpers");
 
+// Register a new customer
 router.post("/register", async (request, response, next) => {
   let newCustomer;
   try {
@@ -21,10 +21,18 @@ router.post("/register", async (request, response, next) => {
     return next(error);
   }
   const accessToken = await generateAccessToken(newCustomer._id);
-
   response
     .status(201)
     .json({ customer: newCustomer, accessToken: accessToken });
+});
+
+// Get all customers (admin only)
+router.get("/", async (request, response, next) => {
+  const customers = await getAllCustomers();
+  response.status(200).json({
+    customers: customers,
+    accessToken: request.headers["authorization"],
+  });
 });
 
 module.exports = router;
