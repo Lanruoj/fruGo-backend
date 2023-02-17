6816404652258b8f10f6c0f8518d493225a052ab
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-17 11:25:25 +1100

    Tidy up authenticateUser()

--- src/controllers/auth/authMiddleware.js
+++ src/controllers/auth/authMiddleware.js
@@ -6,24 +6,32 @@ const { Admin } = require("../../models/Admin");
 async function authenticateUser(request, response, next) {
   let verifiedJWT;
   try {
-    verifiedJWT = verifyJWT(request.body["authorization"]);
-    next();
+    verifiedJWT = verifyJWT(request.headers["authorization"]);
   } catch (error) {
     return next(new Error("Invalid access token"));
   }
   const decryptedUser = decryptString(verifiedJWT.payload.user);
-  const userString = JSON.parse(decryptedUser);
-  const userDocument =
-    (await Customer.findById(user).exec()) ||
-    (await Merchant.findById(user).exec()) ||
-    (await Admin.findById(user).exec());
+  const userObject = JSON.parse(decryptedUser);
+  // Find user subtype
+  const foundCustomer = await Customer.findById(userObject._id).exec();
+  const foundMerchant = await Merchant.findById(userObject._id).exec();
+  const foundAdmin = await Admin.findById(userObject._id).exec();
+  const userDocument = foundCustomer || foundMerchant || foundAdmin;
   if (
-    !userDocument.email == userString.email ||
-    !userDocument.password == userDocument.password
+    !userDocument.email == userObject.email ||
+    !userDocument.password == userObject.password
   ) {
-    return next(new Error("Authentication failed"));
+    const error = new Error("Authentication failed");
+    error.status = 401;
+    return next(error);
   }
+  request.user = userDocument._id;
+  request.role = userDocument.modelName;
   next();
 }
+
+// async function allowAdminOnly(request, response, next) {
+//   request.headers["authorization"];
+// }
 
 module.exports = { authenticateUser };
