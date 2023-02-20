178f8412ea728ec1ff8469b077167cb280b75d6c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-20 14:14:09 +1100

    Create validateCustomerLogin() middleware

--- src/controllers/customers/CustomerMiddleware.js
+++ src/controllers/customers/CustomerMiddleware.js
@@ -1,13 +1,14 @@
 const { Customer } = require("../../models/Customer");
 
 async function validateCustomerLogin(request, response, next) {
-  const foundCustomer = await Customer.find({
+  const foundCustomer = await Customer.findOne({
     email: request.body.email,
   }).exec();
   if (!foundCustomer) throw new Error("Email is incorrect");
   if (!foundCustomer.password == request.body.password) {
     throw new Error("Password is incorrect");
   }
+  request.userID = foundCustomer._id;
   next();
 }
 
