f40390c0cf170ef09e2a23bff31390dbb52e37d5
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-21 16:30:32 +1100

    Check if reference field queried in filterCollection()

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -6,7 +6,12 @@ async function filterCollection(model, queryString) {
   let queryObject = {};
   for (query of queries) {
     const key = query[0];
-    const value = { $regex: new RegExp(query[1], "i") };
+    const valueIsObjectId = mongoose.isValidObjectId(query[1]);
+    if (key[0] == "_" && valueIsObjectId) {
+      value = query[1];
+    } else {
+      value = { $regex: new RegExp(query[1], "i") };
+    }
     queryObject[key] = value;
   }
   const results = await mongoose.model(model).find(queryObject).exec();
