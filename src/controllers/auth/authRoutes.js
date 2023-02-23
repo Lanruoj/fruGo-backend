3406339a4de3dbcc84a8d523ea15cee5325550b0
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-23 11:38:30 +1100

    Add try/catch block to `[POST] /login`

--- src/controllers/auth/authRoutes.js
+++ src/controllers/auth/authRoutes.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const { createCart } = require("../carts/CartHelpers");
 const router = express.Router();
 const { loginUser } = require("./authHelpers");
 const { validateLoginDetails } = require("./authMiddleware");
 
 // Universal login
 router.post("/login", validateLoginDetails, async (request, response, next) => {
-  const accessToken = await loginUser(request.user);
-  response.status(200).json({
-    status: 200,
-    user: request.user,
-    accessToken: accessToken,
-  });
+  try {
+    const accessToken = await loginUser(request.user);
+    response.status(200).json({
+      status: 200,
+      user: request.user,
+      accessToken: accessToken,
+    });
+  } catch (error) {
+    error.status = 401;
+    return next(error);
+  }
 });
 
 module.exports = router;
