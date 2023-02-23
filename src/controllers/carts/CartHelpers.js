6637f8b8cb994addf2d7c6d35abefbdcc1caa528
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-23 11:56:06 +1100

    Create `createCart()` helper

--- src/controllers/carts/CartHelpers.js
+++ src/controllers/carts/CartHelpers.js
@@ -1,5 +1,6 @@
 const { Customer } = require("../../models/Customer");
 const { Cart } = require("../../models/Cart");
+const { Merchant } = require("../../models/Merchant");
 
 async function getCartByCustomerID(customerID) {
   try {
@@ -18,4 +19,21 @@ async function getCartByCustomerID(customerID) {
   }
 }
 
-module.exports = { getCartByCustomerID };
+async function createCart(customerID) {
+  try {
+    const customer = await Customer.findById(customerID).exec();
+    const city = customer._city;
+    const merchant = await Merchant.findOne({ _city: city });
+    const cart = await Cart.create({
+      _customer: customerID,
+      _merchant: merchant,
+    });
+    return cart;
+  } catch (error) {
+    console.log(error);
+    error.status = 400;
+    throw error;
+  }
+}
+
+module.exports = { getCartByCustomerID, createCart };
