f73e09d86fd2b6b8207e7c3d1e3d387729351ffa
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-24 12:59:38 +1100

    Change `corsOptions.origin` to http

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ switch (process.env.NODE_ENV.toLowerCase()) {
 // Configure CORS settings
 const cors = require("cors");
 const corsOptions = {
-  origin: [`https://${HOST}:${PORT}`, "https://frugo-backend.up.railway.app/"],
+  origin: [`http://${HOST}:${PORT}`, "https://frugo-backend.up.railway.app/"],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
