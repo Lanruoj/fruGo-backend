4a9517917f53c27734090bdf472e0431ecb65381
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-24 12:19:52 +1100

    Conditionally set `HOST` & `PORT`

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 // Parse environment variables
 const dotenv = require("dotenv");
 dotenv.config();
-const HOST = process.env.HOST;
-const PORT = process.env.PORT;
 
 // Configure Express server
 const express = require("express");
@@ -24,25 +22,23 @@ app.use(
   })
 );
 
-// Configure CORS settings
-const cors = require("cors");
-const corsOptions = {
-  origin: [`http://${HOST}:${PORT}`],
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-// Configure database URL
-let databaseURL = "";
+// Configure environment variables
+let databaseURL, HOST, PORT;
 switch (process.env.NODE_ENV.toLowerCase()) {
   case "test":
     databaseURL = process.env.TEST_DATABASE_URL;
+    HOST = process.env.DEV_HOST;
+    PORT = process.env.DEV_PORT;
     break;
   case "development":
     databaseURL = process.env.DEV_DATABASE_URL;
+    HOST = process.env.DEV_HOST;
+    PORT = process.env.DEV_PORT;
     break;
   case "production":
     databaseURL = process.env.PRODUCTION_DATABASE_URL;
+    HOST = process.env.PRODUCTION_HOST;
+    PORT = process.env.PRODUCTION_PORT;
     break;
   default:
     console.error(
@@ -50,6 +46,14 @@ switch (process.env.NODE_ENV.toLowerCase()) {
     );
     break;
 }
+
+// Configure CORS settings
+const cors = require("cors");
+const corsOptions = {
+  origin: [`https://${HOST}:${PORT}`],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Connect to database
 const { connectDatabase } = require("./database");
