fb79a7f5e8cc163fd979e00901853df62efef9cd
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-27 12:12:35 +1100

    Use `allowAdminOnly()` for `[GET] /`

--- src/controllers/orders/OrderRoutes.js
+++ src/controllers/orders/OrderRoutes.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateUser } = require("../auth/authMiddleware");
+const { authenticateUser, allowAdminOnly } = require("../auth/authMiddleware");
 const { filterCollection } = require("../helpers");
 const { getAllOrders, getOrderByID } = require("./OrderHelpers");
 
-router.get("/", authenticateUser, async (request, response, next) => {
-  try {
-    const result = await getAllOrders();
-    response.status(200).json({
-      status: 200,
-      data: result,
-    });
-  } catch (error) {
-    return next(error);
+router.get(
+  "/",
+  authenticateUser,
+  allowAdminOnly,
+  async (request, response, next) => {
+    try {
+      const result = await getAllOrders();
+      response.status(200).json({
+        status: 200,
+        data: result,
+      });
+    } catch (error) {
+      return next(error);
+    }
   }
-});
+);
 
 router.get("/:id", authenticateUser, async (request, response, next) => {
   try {
