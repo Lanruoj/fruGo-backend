b088e875c3c58214dd269147aa8b9d147648b4c1
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-27 12:09:03 +1100

    Create `[GET] /:id` route

--- src/controllers/orders/OrderRoutes.js
+++ src/controllers/orders/OrderRoutes.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { authenticateUser } = require("../auth/authMiddleware");
 const { filterCollection } = require("../helpers");
-const { getAllOrders } = require("./OrderHelpers");
+const { getAllOrders, getOrderByID } = require("./OrderHelpers");
 
 router.get("/", authenticateUser, async (request, response, next) => {
   try {
-    const result = await getAllOrders(request.params.id);
+    const result = await getAllOrders();
+    response.status(200).json({
+      status: 200,
+      data: result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get("/:id", authenticateUser, async (request, response, next) => {
+  try {
+    const result = await getOrderByID(request.params.id);
     response.status(200).json({
       status: 200,
       data: result,
