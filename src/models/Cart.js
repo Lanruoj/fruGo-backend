3d4bdba5bc427d6caddb960867971c252a61f518
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-28 12:23:36 +1100

    Add `subTotal` virtual field

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,26 +1,53 @@
 const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema({
-  _customer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Customer",
-    required: true,
-  },
-  _merchant: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Merchant",
-    required: true,
-  },
-  products: [
-    {
-      _stockProduct: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "StockProduct",
-      },
-      subQuantity: { type: Number, default: 1 },
+const CartSchema = new mongoose.Schema(
+  {
+    _customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
     },
-  ],
-  totalPrice: { type: Number, default: 0 },
+    _merchant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Merchant",
+      required: true,
+    },
+    products: [
+      {
+        _stockProduct: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "StockProduct",
+        },
+        subQuantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+CartSchema.pre("findOne", function (next) {
+  this.populate({
+    path: "products",
+    populate: {
+      path: "_stockProduct",
+      model: "StockProduct",
+      populate: { path: "_product", model: "Product" },
+    },
+  });
+  next();
+});
+
+CartSchema.virtual("subTotal").get(function () {
+  let subTotal = 0;
+  for (let cartProduct of this.products) {
+    subTotal +=
+      cartProduct.subQuantity * cartProduct._stockProduct._product.price;
+  }
+  return subTotal;
 });
 
 const Cart = mongoose.model("Cart", CartSchema);
