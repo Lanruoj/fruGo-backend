b1ba94779bc2563278f3a4b17358e71593404a21
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-28 17:08:09 +1100

    Refactor `createOrder()` to update reference arrays

--- src/controllers/orders/OrderHelpers.js
+++ src/controllers/orders/OrderHelpers.js
@@ -2,7 +2,7 @@ const { Order } = require("../../models/Order");
 const { Customer } = require("../../models/Customer");
 const { Merchant } = require("../../models/Merchant");
 const { Cart } = require("../../models/Cart");
-const { createCart } = require("../carts/CartHelpers");
+const { createCart, clearCart } = require("../carts/CartHelpers");
 const { updateStockQuantity } = require("../merchants/MerchantHelpers");
 const { getCustomersMerchant } = require("../customers/CustomerHelpers");
 
@@ -39,21 +39,7 @@ async function getOrderByID(orderID) {
 }
 
 async function getOrdersByCustomerID(customerID, status) {
-  let customer = await Customer.findById(customerID)
-    .populate({
-      path: "orders",
-      populate: {
-        path: "_cart",
-        model: "Cart",
-        populate: {
-          path: "products",
-          populate: { path: "_product", model: "Product" },
-        },
-      },
-    })
-    .exec();
-  let orders = customer.orders;
-  console.log(orders);
+  let orders = await Order.find({ _customer: customerID }).exec();
   if (status) {
     orders = orders.filter((order) => order.status == status);
   }
@@ -85,7 +71,7 @@ async function getOrdersByMerchantID(merchantID, status) {
 async function createOrder(customerID) {
   const cart = await Cart.findOne({ _customer: customerID })
     .populate({
-      path: "products",
+      path: "_cartProducts",
       populate: {
         path: "_stockProduct",
         model: "StockProduct",
@@ -93,21 +79,40 @@ async function createOrder(customerID) {
       },
     })
     .exec();
-  if (!cart.products.length) {
+  if (!cart._cartProducts.length) {
     const error = new Error();
     error.message = ": : Cart is empty";
     error.status = 400;
     throw error;
   }
-  for (cartProduct of cart.products) {
+  for (cartProduct of cart._cartProducts) {
     await updateStockQuantity({
       stockProduct: cartProduct._stockProduct._id,
       quantity: cartProduct._stockProduct.quantity - cartProduct.subQuantity,
     });
   }
-
-  const order = await Order.create({ _cart: cart });
-  await Cart.findOneAndDelete({ _customer: customerID }).exec();
+  const merchant = await getCustomersMerchant(customerID);
+  const order = await Order.create({
+    _customer: customerID,
+    _merchant: merchant._id,
+    _orderProducts: cart._cartProducts,
+  });
+  console.log("order" + order);
+  await Customer.findByIdAndUpdate(
+    order._customer,
+    {
+      $push: { orders: order },
+    },
+    { returnDocument: "after" }
+  );
+  await Merchant.findByIdAndUpdate(
+    order._customer_merchant,
+    {
+      $push: { orders: order },
+    },
+    { returnDocument: "after" }
+  );
+  await Cart.findOneAndDelete({ _customer: customerID });
   await createCart(customerID);
   return order;
 }
