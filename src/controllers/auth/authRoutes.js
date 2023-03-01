ebf3837b3d7c1cc65c6cb7c7c3e26f651e6ff5c0
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 14:05:07 +1100

    Create `[POST] /logout` route

--- src/controllers/auth/authRoutes.js
+++ src/controllers/auth/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { loginUser } = require("./authHelpers");
-const { validateLoginDetails } = require("./authMiddleware");
+const { loginUser, logoutUser } = require("./authHelpers");
+const { validateLoginDetails, authenticateUser } = require("./authMiddleware");
 
 // Universal login
 router.post("/login", validateLoginDetails, async (request, response, next) => {
@@ -12,11 +12,26 @@ router.post("/login", validateLoginDetails, async (request, response, next) => {
     );
     const responseObject = {
       status: 200,
+      message: "You have been logged in",
       user: user,
       accessToken: accessToken,
     };
     if (cart) responseObject.cart = cart;
     response.status(200).json(responseObject);
+  } catch (error) {
+    error.status = 401;
+    console.log(error);
+    return next(error);
+  }
+});
+
+// Universal logout
+router.post("/logout", authenticateUser, async (request, response, next) => {
+  try {
+    const user = await logoutUser(request.user, request.role);
+    response
+      .status(200)
+      .json({ status: 200, message: "You have been logged out", user: user });
   } catch (error) {
     error.status = 401;
     console.log(error);
