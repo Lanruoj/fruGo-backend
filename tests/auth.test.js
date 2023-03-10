5541d55ab7e8495bbb0e0ee278e71f440ed3290d
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-10 16:52:47 +1100

    Delete `auth` test

--- tests/auth.test.js
+++
@@ -1,8 +0,0 @@
-const { hashString } = require("../src/controllers/auth/authHelpers");
-describe("Hashing", () => {
-  it("Hashes a string", async () => {
-    const plainText = "hello1234";
-    const result = await hashString(plainText);
-    expect(result.length).toBeGreaterThan(plainText.length);
-  });
-});
